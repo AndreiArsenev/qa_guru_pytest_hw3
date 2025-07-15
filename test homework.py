6ac7c014943ef4be178ada8211010d0385e4fd5e
AndreiArsenev <3845400@gmail.com>
2025-07-15 12:19:24 +0300

    Домашняя работа на изменение разрешения экрана и поиск

--- test homework.py
+++ test homework.py
@@ -1,22 +1,23 @@
 import pytest
+from selene import browser browser, be, have
 
-from selene import browser
 
 @pytest.fixture(autouse=True)
 def browser_size():
- browser.config.window_height = 1024
- browser.config.window_width = 768
- browser.open('https://duckduckgo.com')
- yield
- browser.quit()
+    browser.config.window_height = 1024
+    browser.config.window_width = 768
+    browser.open('https://duckduckgo.com')
+    yield
+    browser.quit()
+
 
 def test_duckduck_success_selene():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('html').should(have.text('yashaka/selene'))
     print("Тест завершен!")
 
+
 def test_duckduck_not_success():
-    browser.element('[name="q"]').should(be.blank).type(
-        'Енггнйцегунеiuyewuyiqwyeiqyweiqyweiqyweiqwuyeiuqwye').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('Енггнйцегунеiuyewiuqwye').press_enter()
     browser.element('html').should(have.text('ничего не найдено'))
     print('не найдено')
