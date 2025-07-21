71e2f629d5b89dacfd22db332705783425fb8b2b
Andrei Arsenev <3845400@gmail.com>
2025-07-21 16:06:40 +0300

    изменил настройки согласно требованиям requirements

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture(autouse=True)
 def browser_size():
     browser.config.window_height = 1024
-    browser.config.window_width = 768
+    browser.config.window_width = 2000
     browser.open('https://duckduckgo.com')
     yield
     browser.quit()
